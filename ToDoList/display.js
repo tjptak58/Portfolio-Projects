eeea84dc20c4263ee7d2f986cccf78eaa99e8205
Tyler Ptak <tylerptak@gmail.com>
2024-09-19 16:49:20 -0400

    Cleaning

--- ToDoList/display.js
+++
@@ -1,9 +0,0 @@
-function displayDate() {
-    function Person(name, age, haircolor) {
-        this.name = name;
-        this.age = age;
-        this.haircolor = haircolor;
-    };
-    const tj = new Person("TJ", 33, "black");
-    alert(tj.age);
-}
